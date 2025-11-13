c03c824f5262647905d3850bb2c3e2e6288d0686
webfusionintegration <fd-huber@hotmail.com>
2025-11-13 12:42:49 -0500

    Add content type parameter to serveAsset function utf-8

--- workers.js
+++ workers.js
@@ -9,7 +9,7 @@ async function handleRequest(event) {
   const url = new URL(event.request.url)
 
   // Fonction utilitaire pour servir un fichier depuis dist/
-  async function serveAsset(path) {
+  async function serveAsset(path, contentType = 'text/plain; charset=utf-8') {
     try {
       const response = await getAssetFromKV(event, {
         mapRequestToAsset: req => new Request(`${new URL(req.url).origin}${path}`)
@@ -17,6 +17,7 @@ async function handleRequest(event) {
       const duration = Date.now() - startTime
       const newHeaders = new Headers(response.headers)
       newHeaders.set('X-Processing-Time-ms', duration.toString())
+      newHeaders.set('Content-Type', contentType) // <-- UTF-8 ici
       return new Response(response.body, {
         status: response.status,
         statusText: response.statusText,
@@ -38,12 +39,12 @@ async function handleRequest(event) {
 
   // Servir sitemap-video.xml
   if (url.pathname === '/sitemap-video.xml') {
-    return serveAsset('/sitemap-video.xml')
+    return serveAsset('/sitemap-video.xml', 'application/xml; charset=utf-8')
   }
 
   // Servir llms.txt
   if (url.pathname === '/llms.txt') {
-    return serveAsset('/llms.txt')
+    return serveAsset('/llms.txt') // texte avec accents en UTF-8
   }
 
   // Pour tout le reste, laisser passer
